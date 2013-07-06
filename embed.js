af695d0caeefa0deef0035cd798b4b2d3452fbe6
Max Ogden <max@maxogden.com>
2013-07-06 11:31:53 -0700

    update embed

--- embed.js
+++ embed.js
@@ -1,13 +1,14 @@
 var jsonp = require('jsonp')
-var request = require('browser-request')
+var url = require('url')
 
-var parts = window.location.pathname.split('/')
-var gistID = parts[2]
+var parsedURL = url.parse(window.location.href, true)
+var gistID = parsedURL.query.gist
 
 jsonp('https://api.github.com/gists/' + gistID, function(err, gist) {
   if (err) return console.log(err)
   var head = gist.data.files['head.html'].content
   var minified = gist.data.files['minified.js'].content
   document.head.innerHTML += head
-  eval(minified)
+  _eval = eval
+  _eval(minified)
 })
