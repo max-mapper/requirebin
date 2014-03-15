92fb4bb8e1c860faa8c3c289f64bfe9b7de58fd2
Max Ogden <max@maxogden.com>
2014-03-14 17:20:08 -0700

    support user/gistid in embed

--- embed.js
+++ embed.js
@@ -1,7 +1,6 @@
 var jsonp = require('jsonp')
 var url = require('url')
 
-
 var parsedURL = url.parse(window.location.href, true)
 var gistID = parsedURL.query.gist
 
@@ -9,19 +8,37 @@ var binURL = "/?gist=" + gistID
 var link = document.querySelector('.requirebin-link')
 if (link) link.setAttribute('href', binURL)
 
-jsonp('https://api.github.com/gists/' + gistID, function(err, gist) {
-  if (err) return console.log(err)
-  var files = gist.data.files
-  
-  var headFile = files['page-head.html']
-  if (!headFile) headFile = files['head.html']
-  if (headFile) var head = headFile.content
+if (gistID.indexOf('/') > -1) loadRaw(gistID)
+else loadFromAPI(gistID)
+
+function loadFromAPI(gistID) {
+  jsonp('https://api.github.com/gists/' + gistID, function(err, gist) {
+    if (err) return console.log(err)
+    var files = gist.data.files
+    
+    var headFile = files['page-head.html']
+    if (!headFile) headFile = files['head.html']
+    if (headFile) var head = headFile.content
+    
+    var minFile = files['minified.js']
+    if (minFile) var bundle = minFile.content
+    render(head, bundle)
+  })
+}
+
+function loadRaw(gistID) {
+  var bundleURL = "https://gist.githubusercontent.com/" + gistID + "/raw/minified.js"
+  var script = document.createElement('script')
+  script.setAttribute('src', bundleURL)
+  document.head.appendChild(script)
+}
+
+
+function render(head, bundle) {
   if (head) document.head.innerHTML += head
   
-  var minFile = files['minified.js']
-  if (minFile) var minified = minFile.content
-  else var minified = "document.body.innerHTML += 'not a valid requirebin gist - missing minified.js'"
+  if (!bundle) bundle = "document.body.innerHTML += 'not a valid requirebin gist - missing minified.js'"
   
   _eval = eval
-  _eval(minified)
-})
+  _eval(bundle)
+}
