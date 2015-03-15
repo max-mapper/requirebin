9c4aa71b9f6658a5659048e5e980b70e9b80d4e9
Max Ogden <max@maxogden.com>
2015-03-15 16:32:26 -0700

    fix config

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
   GITHUB_CLIENT: '77ca0223691ae7245419',
-  GATEKEEPER: 'http://localhost:9999',
-  BROWSERIFYCDN: 'http://localhost:8000'
+  GATEKEEPER: 'http://gatekeeper.maxogden.com/',
+  BROWSERIFYCDN: 'https://wzrd.in'
 }
