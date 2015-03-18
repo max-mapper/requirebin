796acc1be763ead1f7bdbb7e92621f085227551a
Mauricio Poppe <me@maurizzzio.com>
2015-03-17 21:04:09 -0400

    config.js file updated

--- config.js
+++ config.js
@@ -1,15 +1,16 @@
 // for production:
 
-// module.exports = {
-//   GITHUB_CLIENT: 'f7b1530b019cbb2619d5',
-//   GATEKEEPER: 'http://gatekeeper.maxogden.com',
-//   BROWSERIFYCDN: 'https://wzrd.in'
-// }
-
-// for local dev:
-
-module.exports = {
-  GITHUB_CLIENT: '77ca0223691ae7245419', // redirect goes to localhost:5000
-  GATEKEEPER: 'http://localhost:9999',
-  BROWSERIFYCDN: 'https://wzrd.in'
+var envs = {
+  production: {
+   GITHUB_CLIENT: 'f7b1530b019cbb2619d5',
+   GATEKEEPER: 'http://gatekeeper.maxogden.com',
+   BROWSERIFYCDN: 'https://wzrd.in'
+  },
+  dev: {
+    GITHUB_CLIENT: '77ca0223691ae7245419', // redirect goes to localhost:5000
+      GATEKEEPER: 'http://localhost:9999',
+    BROWSERIFYCDN: 'https://wzrd.in'
+  }
 }
+
+module.exports = envs.dev
