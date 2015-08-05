09da4bb19443c90cf5220dccbd976048bd6f3a52
Feross Aboukhadijeh <feross@feross.org>
2015-08-06 00:52:38 +0200

    standard 5.0.0

--- config.js
+++ config.js
@@ -2,13 +2,13 @@
 
 var envs = {
   production: {
-   GITHUB_CLIENT: 'f7b1530b019cbb2619d5',
-   GATEKEEPER: 'http://gatekeeper.maxogden.com',
-   BROWSERIFYCDN: 'https://wzrd.in'
+    GITHUB_CLIENT: 'f7b1530b019cbb2619d5',
+    GATEKEEPER: 'http://gatekeeper.maxogden.com',
+    BROWSERIFYCDN: 'https://wzrd.in'
   },
   dev: {
     GITHUB_CLIENT: '77ca0223691ae7245419', // redirect goes to localhost:5000
-      GATEKEEPER: 'http://localhost:9999',
+    GATEKEEPER: 'http://localhost:9999',
     BROWSERIFYCDN: 'https://wzrd.in'
   }
 }
